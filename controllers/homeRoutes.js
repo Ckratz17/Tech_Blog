2d880b42e8d1163af8fe204f879b9ab6c4f8933c
Chris Kratz <Ckratz17@gmail.com>
2022-08-14 14:44:01 -0400

    set up path in homeRoutes to render the handlebar pages to begin working on

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,43 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+    try{
+        res.render('homepage')
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/dash', async (req, res) => {
+    try{
+        res.render('dashboard')
+    }catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/blog', async (req, res) => {
+    try{
+        res.render('blog')
+    }catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/login', async (req, res) => {
+    try{
+        res.render('login')
+    }catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get('/newblog', async (req, res) => {
+    try{
+        res.render('singleBlog')
+    }catch (err) {
+        res.status(500).json(err)
+    }
+})
+
 module.exports = router
